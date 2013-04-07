8316f66b289798ee019f3750947db34973279c7a
Rolph Recto <rolph.recto@gmail.com>
2013-04-06 22:40:10 -0400

    Added functionality to strip enclosures

--- markov.py
+++ markov.py
@@ -52,7 +52,6 @@ def create_markov_map(filename, chunk_size):
 
 	return markov_map
 
-
 def generate_text(max_chunks, markov_map, words_per_line):
 	"""generate text sequence of a specified length using a given Markov map"""
 
@@ -119,27 +118,46 @@ def generate_text(max_chunks, markov_map, words_per_line):
 	else:
 		text = " ".join(word_list)
 		
+	return text
+
+def ends_in_punctuation(text):
+	return '.' in text or '?' in text or '!' in text
+	
+def end_sentence(text):
+	"""ensures that the generated text ends in a sentence"""
+
 	#if there is punctuation in the generated text
 	#that denotes a sentence, make sure the text
 	#ends in a complete sentence, not a fragment
-	if '.' in text or '?' in text or '!' in text:
+	if ends_in_punctuation(text):
 		#find the last instance of the punctuation '.', '?' or '!'
-		period_index = text.rfind('.')
-		question_index = text.rfind('?')
-		exclamation_index = text.rfind('!')
-		
-		last_index = period_index
-		if question_index > last_index:
-			last_index = question_index
-		if exclamation_index > last_index:
-			last_index = exclamation_index
+		last_index = 0
+		for punctuation in ['.', '?', '!']:
+			punc_index = text.rfind(punctuation)
+			if last_index < punc_index:
+				last_index = punc_index
 		
 		return text[:last_index+1]
 	#if there is no sentence punctuation, return the whole text
 	else:
 		return text
-
-
+	
+def strip_enclosures(text):
+	"""removes parenthesis and quote-marks from the text to prevent mismatched/open enclosures"""
+	
+	for bad_char in ['\"', '[', ']', '(', ')']:
+		text = text.replace(bad_char, '')
+	
+	return text
+		
+def cleanup(text):
+	"""add decorators to this function to clean up the generated text"""
+	
+	text = end_sentence(text)
+	text = strip_enclosures(text)
+	
+	return text
+	
 def main(filename, num_words, chunk_size, words_per_line):
 	#file must exist!
 	if os.path.exists(filename):
@@ -147,6 +165,11 @@ def main(filename, num_words, chunk_size, words_per_line):
 		if num_words > 0:
 			markov_map = create_markov_map(filename, chunk_size)
 			generated_text = generate_text(num_words, markov_map, words_per_line)
+			
+			#clean up the generated text
+			generated_text = cleanup(generated_text)
+			
+			print len(generated_text)
 			print generated_text
 
 		else:
